d0824e8dd2556eb7f8c71e45d75710523fc537dc
hophoet <hophoet@gmail.com>
2020-02-07 19:59:30 +0000

    update client modal with the user field, migration of allauth app

--- contact/urls.py
+++ contact/urls.py
@@ -10,7 +10,7 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.ContactsListView.as_view(), name='contacts_list'),#contact listing view
     path('<int:client_id>/', views.ContactDetailView.as_view(), name='detail'),#contact detail view
-    path('', views.ClientSearchView.as_view(), name='search_contact'),#client searching view
+    path('search', views.ClientSearchView.as_view(), name='search_contact'),#client searching view
     # path('login/', obtain_auth_token, name='obtain-token'),
     path('add/', views.AddContactView.as_view(), name='add-contact' ),
 ]
