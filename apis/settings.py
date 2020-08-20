b87e975b95165ecb8c102405e16d55e3f8032293
hophoet <hophoet@gmail.com>
2020-08-20 14:12:18 +0000

    added rest framework app to the settings

--- apis/settings.py
+++ apis/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'contact',
+    'rest_framework',
+    'rest_framework.authtoken',
+
 ]
 
 MIDDLEWARE = [
