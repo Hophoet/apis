640ceed78dbd4fb00c468f60eceadaf5214c7ac8
hophoet <hophoet@gmail.com>
2020-08-30 15:53:44 +0000

    update contacts list view(change the contact display), update contact detail view(change contact json display), add add contact view

--- contact/views.py
+++ contact/views.py
@@ -1,31 +1,50 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from django.core.exceptions import ObjectDoesNotExist
 #
 #from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.status import (HTTP_404_NOT_FOUND, HTTP_400_BAD_REQUEST, 
+HTTP_201_CREATED)
 #
-from .serializers import ClientSerializer, CompanySerializer
-from .models import Client
+from .serializers import (ClientSerializer, CompanySerializer,
+ ContactSerializer)
+from .models import Client, Company
+from .forms import AddContactForm, AddContactError
 
-class AllContactView(APIView):
+
+
+#call contact json list builder
+def contactsJsonBuilder(clients):
+    contacts = [ ]
+    for client in clients:
+        contactJson = {
+            'first_name':client.first_name,
+            'last_name':client.last_name,
+            'phone_number':client.phone_number,
+            'email':client.email,
+            'company':{
+                'name':client.company.name
+            }
+        }
+        contacts.append(contactJson)
+    return contacts
     
-    permission_classes = (IsAuthenticated,)
+#single contact json builder
+def contactJsonBuilder(client):
+    contactJson = {
+            'first_name':client.first_name,
+            'last_name':client.last_name,
+            'phone_number':client.phone_number,
+            'email':client.email,
+            'company':{
+                'name':client.company.name
+            }
+        }
 
-    def get(self, request, *args, **kwargs):
-        qs = Client.objects.all()
-       
-        serializer = ClientSerializer(qs, many=True)
-
-        return Response(serializer.data)
-
-    def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+    return contactJson
 
 
 #Contact list view
@@ -36,33 +55,32 @@ class ContactsListView(APIView):
     def get(self, request, *args, **kwargs):
         #get of all the available clients
         clients = Client.objects.all() 
+        contacts = contactsJsonBuilder(clients)
         #building the serializer with the get clients
-        serializer = ClientSerializer(clients, many=True)
         #return the response
-        return Response(serializer.data)
+        return Response(contacts)
 
-    def post(self, request, *args, **kwargs):
-        pass
-        # serializer = PostSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return Response(serializer.data)
+
 
 #contact detail view
 class ContactDetailView(APIView):
     #set of the permission
-    #permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
     #get request method
     def get(self, request, *args, **kwargs):
         #build client and company object with client id
         client_id = kwargs.get('client_id')
-        client = Client.objects.get(id=client_id)
-        company = client.company
+        try:
+            client = Client.objects.get(id=client_id)
+        except ObjectDoesNotExist:
+            return Response(status=HTTP_404_NOT_FOUND, data={'error':'contact not found'})
+
+        contact = contactJsonBuilder(client)
+    
         #build serializers by the get objects
-        client_serializer = ClientSerializer(client)
-        company_serializer = CompanySerializer(company)
+        contact_serializer = ContactSerializer(contact)
         #return the response
-        return Response(data=(client_serializer.data, company_serializer.data))
+        return Response(contact_serializer.data)
 
 #client seart view
 class ClientSearchView(APIView):
@@ -90,3 +108,48 @@ class ClientSearchView(APIView):
         clients = ClientSerializer(clients, many=True)
         #return the response
         return Response(clients.data)
+
+#add contact request errors builder
+def add_contact_request_errors_json_builder(errors_dict):
+    errors_json = {}
+    for key, value in errors_dict:
+        errors_json[key] = {error for error in value}
+    return errors_json
+
+#save contact view
+class AddContactView(APIView):
+    permission_classes = (IsAuthenticated,)
+    
+
+    #post
+    def post(self, request, *args, **kwargs):
+        #create contact from with POST request data
+        add_contact_form = AddContactForm(request.POST, error_class=AddContactError)
+        #contact form not valid case
+        if not add_contact_form.is_valid():
+            #build and return of the error
+            errors_json = add_contact_request_errors_json_builder(add_contact_form.errors.items())
+            return Response(errors_json, status=HTTP_400_BAD_REQUEST)
+        #valid case
+        valid_data = add_contact_form.cleaned_data
+        company_name = valid_data['company']['name']
+        first_name = valid_data['first_name']
+        last_name = valid_data['last_name']
+        email = valid_data['email']
+        phone_number = valid_data['phone_number']
+        #try to get the company (exists case)
+        try:
+            company = Company.objects.get(name=company_name)
+            Client.objects.create(
+                first_name=first_name, last_name=last_name, 
+                email=email, phone_number=phone_number, company=company
+            )
+        #company not exists yet case
+        except ObjectDoesNotExist:
+            company = Company.objects.create(name=company_name)
+            Client.objects.create(
+                first_name=first_name, last_name=last_name, 
+                email=email, phone_number=phone_number, company=company
+            )
+
+        return Response({'detail':'contact added successfully'}, status=HTTP_201_CREATED)
