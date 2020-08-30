db2e38048377a0e00e4dfc9853641d7becc32ed2
hophoet <hophoet@gmail.com>
2020-08-30 15:44:10 +0000

    add Contact serializer

--- contact/serializers.py
+++ contact/serializers.py
@@ -3,6 +3,14 @@ from rest_framework import serializers
 from .models import Client, Company
 
 
+class ContactSerializer(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email = serializers.EmailField()
+    phone_number = serializers.CharField()
+    company = serializers.JSONField()
+
+
 
 #client model serializer 
 class ClientSerializer(serializers.ModelSerializer):
